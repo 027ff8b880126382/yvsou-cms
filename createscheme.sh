8a4eed1774b638b1291c74cb453808c9bdd0f47f
huanglican <lican.huang.hz@gmail.com>
2025-07-02 06:43:49 +0800

    version set  changed

--- createscheme.sh
+++ createscheme.sh
@@ -1,2 +1,12 @@
-mysqldump -u your_user -p --no-data --skip-add-drop-table your_database | sed 's/^CREATE TABLE /CREATE TABLE IF NOT EXISTS /' > install.sql
+mysqldump -u your_user -p --no-data --skip-add-drop-table your_database \
+| sed 's/^CREATE TABLE /CREATE TABLE IF NOT EXISTS /' \
+| sed '1i-- SQL Version: sql-vx.x.x \n-- Generated on: '"$(date -u)" \
+> install.sql
 
+#for mac for example
+mysqldump -u root -p --no-data --skip-add-drop-table yvsou \
+| sed 's/^CREATE TABLE /CREATE TABLE IF NOT EXISTS /' \
+| sed '1i\
+-- SQL Version: sql-v1.0.0\
+-- Generated on: '"$(date -u)" \
+> install.sql
